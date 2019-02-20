084faae3282e1bddca838b47ae4fbba8120403ae
bialek <bialekrafal94@gmail.com>
2019-02-20 21:00:13 +0100

    day 3

--- js/scripts.js
+++ js/scripts.js
@@ -12,64 +12,183 @@ $(document).ready(function() {
         $('nav').toggleClass('active');
     });
 
-    $('.slider').slick({
+    // $('.banners').slick({
+    //     dots: false,
+    //     infinite: true,
+    //     autoplay: true,
+    //     autoplaySpeed: 1000,
+    //     speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear',
+    //     responsive: [{
+    //         breakpoint: 757,
+    //         settings: {
+    //             dots: true,
+    //             draggable: true
+    //         }
+    //     }]
+    // });
+
+    $('.products__wrapper').slick({
+        arrows: false,
         dots: false,
         infinite: true,
         autoplay: true,
         autoplaySpeed: 1000,
         speed: 500,
-        fade: true,
-        cssEase: 'linear',
+        draggable: true,
+        slidesToShow: 4,
         responsive: [{
             breakpoint: 757,
             settings: {
-                dots: true,
-                draggable: true
+                arrows: true,     
+                slidesToShow: 2,
             }
         }]
     });
-
-    var autoPlayTime = 3000;
-    var bannersNumbers = $('.banners div').length - 1;
-    var banner = 1;
-    
-    var moveBanner = function(move = 1) {
-        
-        $('.banners div').eq(banner).css('display', 'none');
-        
-        banner += move;
-        if (banner > bannersNumbers) {
-            banner = 0;
-        }
-        if (banner <=  -1) {
-            banner = bannersNumbers;
-        }
-       
-        $('.banners div').eq(banner).fadeToggle("slow");
    
-    }
-    var autoPlay = function() {
-        setInterval(moveBanner, autoPlayTime);
-    }
-    var autoPlay = function() {
-        intervalId = setInterval(moveBanner, autoPlayTime);
-    }
-    var autoPlayStop = function() {
-        clearInterval(intervalId);
-    }
-    var initBanners = function () {
-        $('.banners div').fadeOut();  
-        autoPlay();
+
+    var slider = function(element) {
+
+        var autoPlayTime = 3000;
+        var bannersNumbers = $(element + ' div').length - 1;
+        var banner = -1;
+
+        var moveBanner = function(move = 1) {      
+            $(element + ' div').eq(banner).css('display', 'none');      
+            banner += move;
+            if (banner > bannersNumbers) {
+                banner = 0;
+            }
+            if (banner <=  -1) {
+                banner = bannersNumbers;
+            }
+            $(element + ' div').eq(banner).fadeToggle("slow");
+        }
+    
+        var autoPlay = function() {
+            intervalId = setInterval(moveBanner, autoPlayTime);
+        }
+    
+        var autoPlayStop = function() {
+            clearInterval(intervalId);
+        }
+    
+        var initBanners = function () {
+            $(element + ' div').fadeOut();  
+            moveBanner();
+            autoPlay();
+        };
+    
+        initBanners();
+    
+        $('#next').on('click', function() {
+            autoPlayStop();
+            moveBanner(1);
+            autoPlay();
+        });
+    
+        $('#prev').on('click', function() {
+            autoPlayStop();
+            moveBanner(-1);
+            autoPlay();
+        });
     };
-    initBanners();
-    $('#next').on('click', function() {
-        autoPlayStop();
-        moveBanner(1);
-        autoPlay();
+
+    slider(".banners");
+
+    // slider(".products");
+    var aktualRequest = function() {
+        var html = "";
+        for(var i=1; i <= 3; i++) {
+            $.ajax({
+                url: "./cms/wpis" + i +".html",
+                dataType: "html",
+                async: false,
+            })
+            .done(function(res) {
+                html += res;
+                if(i >= 3) {
+                    // html.outerHTML;
+                    // var data = { html: html };
+                    localStorage.setItem('aktualności', JSON.stringify(html));
+                }
+            });
+        };
+    };
+
+
+    if (localStorage.aktualności) {
+        aktua = JSON.parse(localStorage.getItem('aktualności'));
+        $('.aktualności').append(aktua);
+    } else {
+        aktualRequest();
+        $('.aktualności').append(aktua);
+        $('.aktualności').slick({
+                arrows: true,
+                dots: false,
+                infinite: true,
+                speed: 500,
+                draggable: true,
+                slidesToShow: 3,
+                responsive: [{
+                    breakpoint: 979,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 757,
+                    settings: {    
+                        slidesToShow: 1,
+                    }
+                }],
+            });
+    }
+
+    $('.info>div .slick-slide').on('click', function() {
+        $('body').append($('<div>', {class: 'modal'}));
+        $(this).clone().appendTo(".modal");
+        $('main').css('display', 'none');
+        $('.modal').append('<button id="close"><i class="fas fa-times"></i></button>')
+        document.querySelector('#close').addEventListener('click', function() {
+            $('.modal').remove();
+            $('main').css('display', 'block');
+        })
     });
-    $('#prev').on('click', function() {
-        autoPlayStop();
-        moveBanner(-1);
-        autoPlay();
-    });
+
+    var ajaxRequest = function(name) {
+        $.ajax({
+            url: "./tabs" + name +".html",
+            dataType: "html",
+            async: false,
+        })
+        .done(function(res) {
+            $('.info').append('<div class="' + name + '"</div>')
+            $('.aktualności').append(res);
+            if(i >= 3) {
+                $('.aktualności').slick({
+                    arrows: true,
+                    dots: false,
+                    infinite: true,
+                    speed: 500,
+                    draggable: true,
+                    slidesToShow: 3,
+                    responsive: [{
+                        breakpoint: 979,
+                        settings: {
+                            slidesToShow: 2,
+                        }
+                    },
+                    {
+                        breakpoint: 757,
+                        settings: {    
+                            slidesToShow: 1,
+                        }
+                    }],
+                });
+            }
+        });
+
+    }
 });
