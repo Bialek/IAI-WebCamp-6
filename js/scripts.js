4d6586d7f29787973d7b08d8b72fde7a6e3cbba6
bialek <bialekrafal94@gmail.com>
2019-02-20 09:10:29 +0100

    day 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,75 @@
 $(document).ready(function() {
-    var self;
-    $('.activatable').click(function() {
-        $(this).toggleClass('active');
-        if (self) {
-            $(self).toggleClass('active');
-            $(self).toggleClass('active');
-        }
-        self = this;
+    $('nav>ul>li').on('click', function() {
+        var mobile = 757;
+        if (parseInt($(window).width()) < mobile) {
+            $('navli').removeClass('active');
+            $(this).toggleClass('active');
+        };
     });
+   
     $('#mobileTrigger').click(function() {
-        $(this).toggleClass('active');
-        $('.nav').toggleClass('active');
+        $(this).toggleClass('open');
+        $('nav').toggleClass('active');
+    });
+
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        responsive: [{
+            breakpoint: 757,
+            settings: {
+                dots: true,
+                draggable: true
+            }
+        }]
+    });
+
+    var autoPlayTime = 3000;
+    var bannersNumbers = $('.banners div').length - 1;
+    var banner = 1;
+    
+    var moveBanner = function(move = 1) {
+        
+        $('.banners div').eq(banner).css('display', 'none');
+        
+        banner += move;
+        if (banner > bannersNumbers) {
+            banner = 0;
+        }
+        if (banner <=  -1) {
+            banner = bannersNumbers;
+        }
+       
+        $('.banners div').eq(banner).fadeToggle("slow");
+   
+    }
+    var autoPlay = function() {
+        setInterval(moveBanner, autoPlayTime);
+    }
+    var autoPlay = function() {
+        intervalId = setInterval(moveBanner, autoPlayTime);
+    }
+    var autoPlayStop = function() {
+        clearInterval(intervalId);
+    }
+    var initBanners = function () {
+        $('.banners div').fadeOut();  
+        autoPlay();
+    };
+    initBanners();
+    $('#next').on('click', function() {
+        autoPlayStop();
+        moveBanner(1);
+        autoPlay();
+    });
+    $('#prev').on('click', function() {
+        autoPlayStop();
+        moveBanner(-1);
+        autoPlay();
     });
 });
